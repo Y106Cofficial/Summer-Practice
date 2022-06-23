59cc65528a62bfe67149513b80bd4037a1703ac8
Roman <92608037+Newtr@users.noreply.github.com>
2022-06-23 12:48:56 +0300

    Beggin

--- Shopkeeper.cpp
+++ Shopkeeper.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+struct Shopkeeper{
+    string name;
+    string secondname;
+    int SaleTime;
+    string specialization;
+
+    float Buy_Sell();
+};
+
+float Shopkeeper::Buy_Sell(){
+    ofstream Ofile;
+    ifstream Ifile;
+    string infoFile;
+    float price;
+    int pos;
+    Ifile.open("Shopkeeper_Info.txt");
+    getline(Ifile,infoFile);
+    pos = infoFile.find("Appels:");
+    infoFile = infoFile.substr(pos,6);
+    cout<<infoFile;
+};
+
 int main(){
-    cout<<"hello";
+    Shopkeeper person1;
+    person1.name="Ivan";
+    person1.secondname = "Petrov";
+    person1.SaleTime = 13;
+    person1.specialization = "Fishman";
+    person1.Buy_Sell();
 }
